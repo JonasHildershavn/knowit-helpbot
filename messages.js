eb876a46678c310896fcbafacf2f5ec8c3445834
JonasHildershavn <jonas.hildershavn@gmail.com>
2023-03-02 19:37:00 +0100

    MAke some changes

--- messages.js
+++ messages.js
@@ -1,14 +1,14 @@
 module.exports = {
   welcome_channel: {
-    text: "Hi there! Bolt is a simple App that forwards messages to another channel by reacting to a message with the :zap: emoji.",
+    text: "Hi there! I am a boy! :zap: emoji.",
     blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "Hi there! \n\n Bolt is a simple App that forwards messages from this channel to <#{{channelId}}|{{channelName}}> by reacting to a message with the :zap: emoji."
-        }
-      }
-    ]
-  }
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Hi there! \n\n I promise that I am a girl, just like I said. <#{{channelId}}|{{channelName}}> TODO: FIx message.",
+        },
+      },
+    ],
+  },
 };
